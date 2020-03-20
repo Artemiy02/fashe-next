2554d82a874a41cb374042adf44275871cb2f5ec
Artem Obzhelyansliy <obzhuk@gmail.com>
2020-03-20 19:45:10 +0200

    fix pribate route

--- components/common/PrivateRoute.tsx
+++ components/common/PrivateRoute.tsx
@@ -7,26 +7,21 @@ import { AuthToken, TOKEN_STORAGE_KEY } from 'services/auth_token';
 import nextCookie from 'next-cookies';
 
 export const PrivateRoute = (Component: NextPage) => {
-  const Wrapper = (pageProps) => <Component {...pageProps} />;
+  const Wrapper = pageProps => <Component {...pageProps} />;
 
-  Wrapper.getInitialProps = async (ctx) => {
-    const state = ctx.store.getState();
+  Wrapper.getInitialProps = async ctx => {
     const tokenInCookie = nextCookie(ctx)[TOKEN_STORAGE_KEY];
 
     const token = new AuthToken(tokenInCookie);
-    console.log('token: ', token.isValid);
 
-    await ctx.store.dispatch(setCurrentUser(token.decodedToken));
     if (token.isValid) {
-    }
-    const isAuth = await authSelector(state);
-
-    if (!isAuth) {
+      await ctx.store.dispatch(setCurrentUser(token.decodedToken));
+    } else {
+      await ctx.store.dispatch(setCurrentUser({}));
       redirect('/login?redirected=true', ctx);
     }
-    const props = Component.getInitialProps
-      ? await Component.getInitialProps(ctx)
-      : {};
+
+    const props = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
     return props;
   };
 
